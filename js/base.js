e19da04fcbfc10f8e7096c1f4c0847ac3230d4d2
symla <symlamain@googlemail.com>
2017-11-14 16:18:02 +0100

    new stage

--- js/base.js
+++ js/base.js
@@ -6,15 +6,18 @@ var imgContainer = null;
 var startIndex = 1;
 var endIndex = 10;
 
+var body = null;
+
 function init() {
 	//menuImgWrapper = document.getElementById('menuImgWrapper');
 	//navBar = document.getElementById('navBar');
 
+	body = document.getElementById('body');
 	imgWrapper = document.getElementById('imgWrapper');
 	imgContainer = document.getElementById('imgContainer');
 
 	//menuImgWrapper.addEventListener("click", toggleMenu);
-	imgWrapper.addEventListener('resize', resizeWrapper);
+	body.addEventListener('resize', resizeWrapper);
 	console.log("success");
 }
 
